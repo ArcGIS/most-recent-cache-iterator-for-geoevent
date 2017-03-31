34bfff4cb0d0a82861052ffa328480b07422d2c8
Morakot Pilouk <mpilouk@esri.com>
2017-03-31 16:43:26 -0700

    Updated to 10.4.x
    Updated to 10.4.x

--- most-recent-cache-iterator-processor/src/main/java/com/esri/geoevent/processor/mostrecentcacheiterator/MostRecentCacheIteratorDefinition.java
+++ most-recent-cache-iterator-processor/src/main/java/com/esri/geoevent/processor/mostrecentcacheiterator/MostRecentCacheIteratorDefinition.java
@@ -17,52 +17,52 @@ import com.esri.ges.processor.GeoEventProcessorDefinitionBase;
 
 public class MostRecentCacheIteratorDefinition extends GeoEventProcessorDefinitionBase
 {
-	private static final BundleLogger							LOGGER				= BundleLoggerFactory.getLogger(MostRecentCacheIteratorDefinition.class);
+  private static final BundleLogger LOGGER = BundleLoggerFactory.getLogger(MostRecentCacheIteratorDefinition.class);
 
-	public MostRecentCacheIteratorDefinition()
-	{
-		try
-		{
-			propertyDefinitions.put("cycleInterval", new PropertyDefinition("cycleInterval", PropertyType.Long, 1000, "Cycle Interval (milliseconds)", "Cycle Interval (milliseconds)", false, false));
-			propertyDefinitions.put("messageInterval", new PropertyDefinition("messageInterval", PropertyType.Long, 10, "Message Interval (milliseconds)", "Message Interval (milliseconds)", false, false));
-			propertyDefinitions.put("clearCacheOnStart", new PropertyDefinition("clearCacheOnStart", PropertyType.Boolean, false, "Clear Cache on Start", "Clear Cache on Start", true, false));
-			propertyDefinitions.put("autoClearCache", new PropertyDefinition("autoClearCache", PropertyType.Boolean, false, "Automatic Clear Cache", "Auto Clear Cache", true, false));
-			propertyDefinitions.put("clearCacheTime", new PropertyDefinition("clearCacheTime", PropertyType.String, "00:00:00", "Clear cache time", "Clear cache time", "autoClearCache=true", false, false));
-		}
-		catch (Exception error)
-		{
-			LOGGER.error("INIT_ERROR", error.getMessage());
-			LOGGER.info(error.getMessage(), error);
-		}
-	}
+  public MostRecentCacheIteratorDefinition()
+  {
+    try
+    {
+      propertyDefinitions.put("cycleInterval", new PropertyDefinition("cycleInterval", PropertyType.Long, 1000, "Cycle Interval (milliseconds)", "Cycle Interval (milliseconds)", false, false));
+      propertyDefinitions.put("messageInterval", new PropertyDefinition("messageInterval", PropertyType.Long, 10, "Message Interval (milliseconds)", "Message Interval (milliseconds)", false, false));
+      propertyDefinitions.put("clearCacheOnStart", new PropertyDefinition("clearCacheOnStart", PropertyType.Boolean, false, "Clear Cache on Start", "Clear Cache on Start", true, false));
+      propertyDefinitions.put("autoClearCache", new PropertyDefinition("autoClearCache", PropertyType.Boolean, false, "Automatic Clear Cache", "Auto Clear Cache", true, false));
+      propertyDefinitions.put("clearCacheTime", new PropertyDefinition("clearCacheTime", PropertyType.String, "00:00:00", "Clear cache time", "Clear cache time", "autoClearCache=true", false, false));
+    }
+    catch (Exception error)
+    {
+      LOGGER.error("INIT_ERROR", error.getMessage());
+      LOGGER.info(error.getMessage(), error);
+    }
+  }
 
-	@Override
-	public String getName()
-	{
-		return "MostRecentCacheIterator";
-	}
+  @Override
+  public String getName()
+  {
+    return "MostRecentCacheIterator";
+  }
 
-	@Override
-	public String getDomain()
-	{
-		return "com.esri.geoevent.processor";
-	}
+  @Override
+  public String getDomain()
+  {
+    return "com.esri.geoevent.processor";
+  }
 
-	@Override
-	public String getVersion()
-	{
-		return "10.3.0";
-	}
+  @Override
+  public String getVersion()
+  {
+    return "10.4.0";
+  }
 
-	@Override
-	public String getLabel()
-	{
-		return "${com.esri.geoevent.processor.most-recent-cache-iterator-processor.PROCESSOR_LABEL}";
-	}
+  @Override
+  public String getLabel()
+  {
+    return "${com.esri.geoevent.processor.most-recent-cache-iterator-processor.PROCESSOR_LABEL}";
+  }
 
-	@Override
-	public String getDescription()
-	{
-		return "${com.esri.geoevent.processor.most-recent-cache-iterator-processor.PROCESSOR_DESC}";
-	}
+  @Override
+  public String getDescription()
+  {
+    return "${com.esri.geoevent.processor.most-recent-cache-iterator-processor.PROCESSOR_DESC}";
+  }
 }
